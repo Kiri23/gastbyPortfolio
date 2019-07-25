bcfed44fc467da45762ee2b232e0ff773036c386
rennehir <git@rennehir.fi>
2019-07-25 09:53:55 +0300

    Get content to service pages

--- theme/src/templates/service.js
+++ theme/src/templates/service.js
@@ -1,13 +1,40 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
 
 import Layout from "../components/layout"
 
-const ServiceTemplate = () => {
+const ServiceTemplate = ({
+  pathContext: { previous, next },
+  data: { service },
+}) => {
+  const { title, body } = service
+
   return (
     <Layout>
-      <h1>Service</h1>
+      <h1>{title}</h1>
+      <MDXRenderer>{body}</MDXRenderer>
+      {previous && (
+        <span>
+          Previous: <Link to={previous.slug}>{previous.title}</Link>
+        </span>
+      )}
+      {next && (
+        <span>
+          Next: <Link to={next.slug}>{next.title}</Link>
+        </span>
+      )}
     </Layout>
   )
 }
 
 export default ServiceTemplate
+
+export const query = graphql`
+  query($id: String!) {
+    service(id: { eq: $id }) {
+      title
+      body
+    }
+  }
+`
--- theme/src/templates/services.js
+++ theme/src/templates/services.js
@@ -1,16 +1,36 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 
-const ServicesTemplate = () => {
+const ServicesTemplate = ({ data: { services } }) => {
   return (
     <Layout>
       <h1>Services</h1>
       <section>
-        <h2>List here</h2>
+        {services.nodes.map(({ id, title, slug, excerpt }) => (
+          <article key={id}>
+            <h2>{title}</h2>
+            <p>{excerpt}</p>
+            <Link to={slug}>Read more</Link>
+          </article>
+        ))}
       </section>
     </Layout>
   )
 }
 
 export default ServicesTemplate
+
+export const query = graphql`
+  query {
+    services: allService {
+      nodes {
+        id
+        title
+        slug
+        excerpt
+      }
+    }
+  }
+`
