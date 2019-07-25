1632c6b19825f3aff52ded7b0879fbf6932f02ba
rennehir <git@rennehir.fi>
2019-07-25 11:56:11 +0300

    Add missing alt

--- demo/src/pages/404.js
+++ demo/src/pages/404.js
@@ -5,7 +5,11 @@ import NotFound from "../../content/assets/undraw_page_not_found_su7k.svg"
 const NotFoundPage = () => {
   return (
     <Layout>
-      <img src={NotFound} style={{ maxWidth: "80%", display: 'inherit', margin: 'auto' }} />
+      <img
+        src={NotFound}
+        alt="Page not found"
+        style={{ maxWidth: "80%", display: "inherit", margin: "auto" }}
+      />
     </Layout>
   )
 }
