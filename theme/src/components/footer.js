33dbd205a0cfddb3495016f314f63c604f0203b2
rennehir <git@rennehir.fi>
2019-07-26 16:09:07 +0300

    Start working on footer

--- theme/src/components/footer.js
+++ theme/src/components/footer.js
@@ -1,12 +1,34 @@
 /** @jsx jsx */
 import { jsx, Styled } from "theme-ui"
+import { useStaticQuery, graphql } from "gatsby"
 
 const Footer = () => {
+  const {
+    footerYaml: { social_media },
+  } = useStaticQuery(graphql`
+    query {
+      footerYaml {
+        social_media {
+          name
+          url
+        }
+      }
+    }
+  `)
   return (
     <div
       sx={{ p: 3, width: "100%", backgroundColor: "primary", color: "white" }}
     >
       <h1>Footer</h1>
+      <div>
+        {social_media.map(media => (
+          <div>
+            <a href={media.url} rel="noreferrer noopener">
+              {media.name}
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
