dd4a3f08cbaf3223d292b45661b3a576c20caf62
rennehir <git@rennehir.fi>
2019-07-31 20:35:22 +0300

    edit footer

--- theme/src/components/footer.js
+++ theme/src/components/footer.js
@@ -1,36 +1,76 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui"
+import { jsx, Styled } from "theme-ui"
 import { useStaticQuery, graphql } from "gatsby"
 
 const Footer = () => {
   const {
-    footerYaml: { social_media },
+    footerYaml: { facebook, twitter, github, linkedin },
   } = useStaticQuery(graphql`
     query {
       footerYaml {
-        social_media {
-          name
-          url
-        }
+        facebook
+        github
+        linkedin
+        twitter
       }
     }
   `)
   return (
     <div
-      sx={{ p: 3, width: "100%", backgroundColor: "primary", color: "white" }}
+      sx={{
+        p: 3,
+        width: "100%",
+        backgroundColor: "primary",
+        color: "white",
+        textAlign: "center",
+      }}
     >
-      <h1>Footer</h1>
       <div>
-        {social_media.map(media => (
-          <div>
-            <a href={media.url} rel="noreferrer noopener">
-              {media.name}
-            </a>
-          </div>
-        ))}
+        {facebook && (
+          <Link to={facebook}>
+            <i className="fab fa-facebook" />
+          </Link>
+        )}
+        {linkedin && (
+          <Link to={facebook}>
+            <i className="fab fa-linkedin" />
+          </Link>
+        )}
+        {github && (
+          <Link to={facebook}>
+            <i className="fab fa-github" />
+          </Link>
+        )}
+        {twitter && (
+          <Link to={facebook}>
+            <i className="fab fa-twitter" />
+          </Link>
+        )}
       </div>
+      <Styled.p sx={{ fontSize: 1 }}>
+        © All rights are reserved | {new Date().getFullYear()} |{" "}
+        <a
+          href="https://smakosh.com/?ref=gatsby-theme-portfolio"
+          rel="noopener noreferrer"
+          target="_blank"
+          sx={{ color: "white" }}
+        >
+          Renne Hirsimäki
+        </a>
+      </Styled.p>
     </div>
   )
 }
 
 export default Footer
+
+const Link = ({ to, children }) => (
+  <a
+    href={to}
+    target="_blank"
+    rel="noopener noreferrer"
+    sx={{ fontSize: 5, color: "white", mx: 2, ":hover": { color: "#D3D3D3" } }}
+  >
+    {children}
+  </a>
+)
